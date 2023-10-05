e81e13faa2b6e71e33173cfec081b9d700d601e4
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2023-10-05 11:58:26 -0300

    Error al abrir archivo y calculo de tamaño

--- Tareas/T4/T4v2/definir.c
+++ Tareas/T4/T4v2/definir.c
@@ -8,6 +8,19 @@
 /*... define variables globales o funciones adicionales ...*/
 
 int main(int argc, char *argv[]) {
-    
+    //Abrimos el archivo en modo r+
+    FILE *in = fopen("dicc.txt","r+");
+    //Comprobamos que el archivo es válido, sino retornamos error
+    if (in == NULL){
+        perror("El archivo no puede abrirse");
+        exit(1);
+    }
+    //Calculamos el tamaño del archivo
+    fseek(in, 0, SEEK_END);
+    int sizeBytes= ftell(in);
+    printf("El tamaño es: %d\n", sizeBytes);
+    return 0;
+
+
 
 }
