3f13f0de4595d05446ef63f0732f602e0791ecc7
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2023-10-09 22:52:58 -0300

    entrega final T4

--- Tareas/T4/T4v2/definir.c
+++ Tareas/T4/T4v2/definir.c
@@ -10,90 +10,94 @@ void insertar(char *nom_dicc,char *llave, char *def);
 
 
 int main(int argc, char *argv[]) {
-   
+    //Corroboramos que la/el cantidad/tipo de argumentos es correcta/o
+    if(argc != 4){
+        fprintf(stderr, "Uso: ./definir <diccionario> <llave> <definicion>\n"); 
+        exit(1);   
+    }
+    //Utilizamos la función auxiliar insertar
     insertar(argv[1], argv[2], argv[3]);
     return 0;
 }
 
 void insertar(char *nom_dicc, char *llave, char *def) {
-    // Abrimos el archivo en modo r+
+    //Abrimos el archivo en modo r+
     FILE *in = fopen(nom_dicc, "r+");
-    
-    // Comprobamos que el archivo es válido, sino retornamos error
+    //Verificamos si el archivo existe
     if (in == NULL) {
-        perror("El archivo no puede abrirse");
+        perror(nom_dicc);
         exit(1);
     }
-    
-    // Calculamos el tamaño del archivo en líneas
+    //Calculamos su tamaño y cantidad de líneas
     fseek(in, 0, SEEK_END);
     int sizeBytes = ftell(in);
     int lineas = sizeBytes / 81;
-    
-    // Definimos el índice de la llave
+    //Vemos el índice preeliminar de la línea para la llave
     int i_llave = hash_string(llave) % lineas;
+    //Variable que nos dirá si es que se recorrió el diccionario completo
     int vueltas = 0;
-    
-    // Buscamos la línea de dicho índice
-    fseek(in, i_llave * 81, SEEK_SET);
-    
-    // Vemos si ya existe la llave a insertar
+    //Creamos un buffer para poder almacenar una línea completa incluyendo \n
     char buf[81];
-    
-    // Leemos toda la línea
-    fread(buf, 1, sizeof(buf), in);
-    
-    // Si la línea está vacía, debería comenzar con un espacio en blanco
-    if (buf[0] == ' ') {
-        // Rebobinamos para escribir sobre la misma línea
-        fseek(in, i_llave * 81, SEEK_SET);
-    } else {
-        while (1) {
-            if (i_llave == lineas) {
-                i_llave = 0;
-                vueltas++;
-                if (vueltas == 2) {
-                    fprintf(stderr, "El diccionario está lleno\n");
-                    fclose(in);
-                    exit(3);
-                    break;
-                }
-            }
-            
-            // Si la línea no está vacía, verificamos si es la misma llave
-            char *llave1 = buf;
-            while (*llave1 != ':') {
-                llave1++;
-            }
-            *llave1 = '\0'; // Terminamos la cadena en ':' para comparar
-            if (strcmp(buf, llave) == 0) {
-                fprintf(stderr, "La llave %s ya se encuentra en el diccionario\n", llave);
+
+    while (1) {           
+        //Revisamos si el archivo ya se leyó por completo
+        if (i_llave == lineas) {
+            i_llave = 0;
+            vueltas++;
+            //En caso de que se hayan dado dos vueltas es porque el diccionario ya está lleno
+            if (vueltas == 2) {
+                fprintf(stderr, "%s: el diccionario esta lleno\n",nom_dicc);
                 fclose(in);
-                exit(2); // Cierra el archivo antes de retornar
-            } else {
+                exit(3);
                 break;
             }
-            
-            // Si no es la misma llave, avanzamos a la siguiente línea de manera circular
-            i_llave++;
-            fseek(in, i_llave * 81, SEEK_SET);
         }
+        //Nos dirigimos hacia la línea preeliminar
+        fseek(in, i_llave * 81, SEEK_SET);
+        //Leemos la línea completa y la almacenamos en el buffer
+        fgets(buf, sizeof(buf), in);
+        //Revisamos si el primer caracter es un espacio en blanco, pues por sintaxis del diccionario,
+        //si ese espacio está vacío, la línea completa debería estarlo.
+        if (buf[0] == ' ') {
+            //Devolvemos el cursor a la posición inicial 
+            fseek(in, i_llave * 81, SEEK_SET);
+            break;
+        }
+        //Copiamos la llave para luego compararla
+        char *llave1 = buf;
+        while (*llave1 != ':') {
+            llave1++;
+        }
+        //Como llegamos al caracter :, ya copiamos toda la llave así que debemos terminar el string.
+        *llave1 = '\0';
+        //Comparamos si son iguales
+        if (strcmp(buf, llave) == 0) {
+            fprintf(stderr, "La llave %s ya se encuentra en el diccionario\n", llave);
+            fclose(in);
+            exit(2);
+        } 
+        i_llave++;
     }
-    
-    // Rellenamos con espacios en blanco hasta llegar a 80 caracteres
+
     int len = strlen(llave) + strlen(def);
-    if (len + 1 < 80) { // Añadimos 1 para el salto de línea
+    if (len + 1 < 80) {
         fprintf(in, "%s:%s", llave, def);
-        for (int i = 0; i < 80 - len - 1; i++) {
+        
+        // Calcular cuántos espacios en blanco necesitamos para completar la línea
+        int espacios = 80 - len - 1;
+        
+        // Rellenar con espacios en blanco
+        for (int i = 0; i < espacios; i++) {
             fprintf(in, " ");
         }
+        
         fprintf(in, "\n");
     } else {
-        // Caso donde la definición es demasiado larga
         perror("La definición es demasiado larga");
         exit(3);
     }
     
     fclose(in);
 }
+
 
