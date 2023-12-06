2cec950a0b9a9a9e3f5999f865bc28a133e620fb
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2023-12-06 19:38:52 -0300

    Enunciado T8

---
+++ Tareas/T8/20231125133220C4258B88E299E6__t8/T8/mas-grandes.c
@@ -0,0 +1,20 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "pss.h"
+       
+// Defina aca la estructura para guardar nombre y tamanno
+
+// Agregue aca las funciones y variables globales adicionales que necesite
+
+int main(int argc, char *argv[]) {
+  // ... complete ...
+}
